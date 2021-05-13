056ecc4bc39b6790a0fdd1467ca2f2203bee782e
Raveryk <raveryk@gmail.com>
2021-05-13 13:42:36 -0500

    Added saga addImages function

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -4,11 +4,11 @@ const axios = require('axios');
 const { response } = require('express');
 
 // set up server file to look at the .env file
-require('dontenv').config();
+require('dotenv').config();
 
 const GIPHY_KEY = process.env.GIPHY_API_KEY;
 
-app.get('/search', (req, res) => {
+router.get('/search', (req, res) => {
     axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${GIPHY_KEY}`)
     .then(response => {
         console.log(`Data from giphy:`, response.data);
