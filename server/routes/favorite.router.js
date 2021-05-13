a066c90fc4957ee426c230063d551e66b2f45057
Robyn Noble <robynhurd09@gmail.com>
2021-05-13 13:56:34 -0500

    updated post router on favorites

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -11,8 +11,20 @@ router.get('/', (req, res) => {
 // add a new favorite
 router.post('/', (req, res) => {
   const newFavorite = req.body;
-  const queryText = `INSERT INTO`
-  res.sendStatus(200);
+  const queryText = `INSERT INTO favorite ("id", "category_id", "image_url")
+                    VALUES ($1, $2, $3)`;
+  const queryValues = [
+      newFavorite.id,
+      newFavorite.category_id,
+      newFavorite.image_url,
+  ];
+  pool.query(queryText, queryValues)
+    .then(() => {res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error completing SELECT favorite query`, error);
+      res.sendStatus(500);
+    });
 });
 
 // update given favorite with a category id
